f8e88daabe318161b386c2d4f3f64cea35ffe518
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-02-03 21:16:25 +0300

    program.cs updated filters and error handler added

--- RestAPI/Program.cs
+++ RestAPI/Program.cs
@@ -1,18 +1,23 @@
 using BuberDinner.Application;
-using BuberDinner.Application.Services.Authentication;
 using BuberDinner.Infrastructure;
+using BuberDinner.RestAPI.Errors;
+using BuberDinner.RestAPI.Filters;
 using BuberDinner.RestAPI.Middleware;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
+    //builder.Services.AddControllers(opt => opt.Filters.Add<ErrorHandlingFilterAttribute>());
     builder.Services.AddControllers();
+    builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
     builder.Services.AddApplication();
     builder.Services.AddInfrastructure(builder.Configuration);
 }
 
 var app = builder.Build();
 {
-    app.UseMiddleware<ErrorHandlingMiddleware>();
+    //app.UseMiddleware<ErrorHandlingMiddleware>();
+    app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
