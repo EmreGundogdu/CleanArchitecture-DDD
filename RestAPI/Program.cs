cef280f652e039e1514d54ac3e9a28b9b743b200
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-02-04 16:42:38 +0300

    program.cs

--- RestAPI/Program.cs
+++ RestAPI/Program.cs
@@ -1,22 +1,20 @@
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
-using BuberDinner.RestAPI.Errors;
+using BuberDinner.RestAPI.Common.Errors;
 using BuberDinner.RestAPI.Filters;
 using BuberDinner.RestAPI.Middleware;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
-    //builder.Services.AddControllers(opt => opt.Filters.Add<ErrorHandlingFilterAttribute>());
     builder.Services.AddControllers();
-    builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
     builder.Services.AddApplication();
     builder.Services.AddInfrastructure(builder.Configuration);
+    builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
 }
 
 var app = builder.Build();
 {
-    //app.UseMiddleware<ErrorHandlingMiddleware>();
     app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
