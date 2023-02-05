a4029ade47e464b8868bf0a17b816f17cd1ecf16
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-02-05 18:00:26 +0300

    Different properties of two model mapped each other with mapster and added AuthenticationMappingConfig

---
+++ RestAPI/Common/Mapping/AuthenticationMappingConfig.cs
@@ -0,0 +1,14 @@
+﻿using BuberDinner.Application.Authentication.Common;
+using BuberDinner.Contracts.Authentication;
+using Mapster;
+
+namespace BuberDinner.RestAPI.Common.Mapping
+{
+    public class AuthenticationMappingConfig : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<AuthenticationResult, AuthenticationResponse>().Map(dest => dest.Token, src => src.Token).Map(dest => dest, src => src.User);
+        }
+    }
+}
